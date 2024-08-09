36560e5fd81da959f5494b5c12aa1c4e46b60134
MixDream <max_juldashev@mail.ru>
2024-08-09 15:26:03 +0300

    Update dz.py

--- dz.py
+++ dz.py
@@ -7,23 +7,35 @@ class Product:
     def __str__(self):
         return f"{self.name}, {self.weight}, {self.category}"
 
+    def __eq__(self, other):
+        return self.name == other.name and self.weight == other.weight and self.category == other.category
+
+    def to_string(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+
 
 class Shop:
     def __init__(self):
         self.__file_name = 'products.txt'
+        try:
+            with open(self.__file_name, 'r') as f:
+                self.products = [Product(*line.strip().split(', ')) for line in f.readlines()]
+        except FileNotFoundError:
+            self.products = []
 
-    def get_products(self):
-        with open(self.__file_name, 'r') as f:
-            return f.read()
+    def save_products(self):
+        with open(self.__file_name, 'w') as f:
+            for product in self.products:
+                f.write(product.to_string() + '\n')
 
     def add(self, *products):
-        products_in_file = self.get_products().split('\n')
         for product in products:
-            if product.name not in products_in_file:
-                with open(self.__file_name, 'a') as f:
-                    f.write(f"{product}\n")
+            if product not in self.products:
+                self.products.append(product)
             else:
                 print(f"Продукт {product.name} уже есть в магазине")
+
+        self.save_products()
 
 
 s1 = Shop()
@@ -31,8 +43,7 @@ p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2)
-
 s1.add(p1, p2, p3)
 
-print(s1.get_products())
+for product in s1.products:
+    print(product)
